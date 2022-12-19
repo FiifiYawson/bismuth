66349654f0482aa3af9d93fea30092488491a1e4
ghyawson@gmail.com <Fiifi Yawson>
2022-12-19 16:03:37 +0000

    rework

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+const questions = Array.from(document.getElementsByClassName("question"))
+const menuOptions = Array.from(document.getElementsByClassName("menu-option"))
+const menuNavUnderline = document.getElementById("menu-nav-underline")
+let activeMenu = document.getElementById("active-menu")
+
+function menuToggle() {
+    menuNavUnderline.style.width = `${activeMenu.clientWidth}px`
+    menuNavUnderline.style.left = `${activeMenu.offsetLeft}px`
+}
+
+questions.forEach(question => {
+    question.addEventListener("click", (e) => {
+        e.target.classList.toggle("active")
+    })
+})
+
+menuOptions.forEach(menuOption => {
+    menuOption.addEventListener("click", (e) => {
+        e.target.id = "active-menu"
+        activeMenu.id = ""
+        activeMenu = e.target
+
+        menuToggle()
+    })
+})
+
+window.addEventListener("resize", (e) => {
+    menuToggle()
+})
+
+menuToggle()
